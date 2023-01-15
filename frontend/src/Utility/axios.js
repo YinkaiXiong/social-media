aa0925cb562b86439c0b2b188ca5b422bf2c9894
Yinkai <tonyxiong995@gmail.com>
2023-01-14 22:34:17 -0500

    Railway hosting url modify

--- frontend/src/Utility/axios.js
+++ frontend/src/Utility/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://social-media-production-30d3.up.railway.app/",
+  baseURL: "https://social-media-production-30d3.up.railway.app",
 });
 
 export default instance;
